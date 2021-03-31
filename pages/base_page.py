f38919b35a25a93ab0f96d7ddc056300f1aa4d55
alexandr.miheev <zoom906@gmail.com>
2021-03-31 21:38:04 +0300

    Test to check the login link

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
@@ -8,3 +9,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+def __init__(self, browser, url, timeout=10):
+    self.browser = browser
+    self.url = url
+    self.browser.implicitly_wait(timeout)
